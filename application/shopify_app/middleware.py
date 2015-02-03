7134210c728e787d12592c1cbf7e6b186bc55be8
bohdan.anderson@gmail.com <bohdan.anderson@gmail.com>
2015-02-03 14:58:40 -0500

    adding details

--- application/shopify_app/middleware.py
+++ application/shopify_app/middleware.py
@@ -10,8 +10,8 @@ class LoginProtection(object):
         if not settings.SHOPIFY_API_KEY or not settings.SHOPIFY_API_SECRET:
             raise ConfigurationError("SHOPIFY_API_KEY and SHOPIFY_API_SECRET must be set in settings")
         shopify.Session.setup(api_key=settings.SHOPIFY_API_KEY,
-                              secret=settings.SHOPIFY_API_SECRET)        shopify.Session.setup(api_key=settings.SHOPIFY_API_KEY,
                               secret=settings.SHOPIFY_API_SECRET)
+
     def process_view(self, request, view_func, view_args, view_kwargs):
         if hasattr(request, 'session') and 'shopify' in request.session:
             print request.session['shopify']['shop_url']
